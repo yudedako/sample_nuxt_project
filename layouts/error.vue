c9233b4d049b1e32a22dba627552eb72bb3ff7dd
yudedako <pluto.club@gmail.com>
2018-09-02 10:19:09 +0900

    error用layoutを追加

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <template v-if="isNotFount">
+      <h1>404 not fount</h1>
+      <nuxt-link to="/"> Back to home </nuxt-link>
+    </template>
+    <template v-else>
+      <h1>Error</h1>
+      <nuxt-link to="/"> Back to home </nuxt-link>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  components: {
+    isNotFound() {
+      return this.error.statusCode == 404
+    }
+  }
+}
+</script>
